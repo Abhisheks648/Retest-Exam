cb14ff5017b3b6aa223056a900613e1db4f37a78
saiabhishektata <abhisheks648@gmail.com>
2023-02-01 02:29:29 +0530

    second commit

---
+++ SI.py
@@ -0,0 +1,15 @@
+# Simple interest in python
+# Enter the amount
+def simpleInterset(A, Y, R):
+    # Calculate the simple interest
+    SI = (A*Y*R)/100
+    return SI
+
+
+A = int(input("Enter the amount: "))
+# Enter the number of years
+Y = int(input("Enter the number of years: "))
+# Enter the rate of interest
+R = float(input("Enter the rate of interest: "))
+
+print("The simple interest is:", simpleInterset(A, Y, R))
